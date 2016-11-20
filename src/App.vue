22fffa2c4c354b462bb6550b1890481b3cba01d8
Kosta Dunn <kostacoffee@gmail.com>
2016-11-20 17:13:44 +1100

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+	h1 {{msg}}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
